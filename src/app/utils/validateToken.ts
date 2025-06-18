013cdb47cd8058fbf1220a20dfa8d5eb9d5659f4
RuzarDev <urazov.ruzar@inbox.ru>
2025-06-18 07:21:25 +0500

    fix

--- src/app/utils/validateToken.ts
+++ src/app/utils/validateToken.ts
@@ -4,7 +4,7 @@ import api from "@/data/dataBase";
 
 export const validateToken = async () => {
   try {
-    const res = await api.post('/auth/validateToken',{},{withCredentials:true});
+    const res = await api.post('/auth/validateToken',{});
     return res;
   } catch (error: any) {
     console.error('Error validating token:', error.response || error.message);
